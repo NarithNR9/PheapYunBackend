08894f3d9b216c689a701c4d3288f3572bdd7777
NarithNR9 <79370506+NarithNR9@users.noreply.github.com>
2023-01-17 14:25:08 +0700

    add klas

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-console.log('hii')
-console.log('hey')
+const express = require('express')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+
+const PORT = process.env.PORT || 5000
+
+connectDB()
+const app = express()
+
+app.use(bodyParser.json())
+
+app.listen(PORT)
+
