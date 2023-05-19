31a4445687daecdd394df1407c3a653583d5347f
NarithNR9 <narith.neymar.jr@gmail.com>
2023-05-19 15:36:34 +0700

    apply cors()

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
 const userRoute = require('./routes/user.route')
 const movieRoute = require('./routes/movie.route')
+var cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE') // or '*' for all method
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-  next()
-})
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE') // or '*' for all method
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+//   next()
+// })
 
 app.use(bodyParser.json())
 
