903fcec241bcb4ffa40fe7b5adca178052b7ae50
NarithNR9 <narith.neymar.jr@gmail.com>
2023-05-19 16:04:19 +0700

    cors fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
 const userRoute = require('./routes/user.route')
 const movieRoute = require('./routes/movie.route')
-var cors = require('cors')
+// var cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
@@ -12,9 +12,9 @@ const app = express()
 // app.use(cors())
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://pheapyun.netlify.app')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE') // or '*' for all method
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Headers', '*')
   next()
 })
 
